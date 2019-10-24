793abff2fbb6e786e666bbbf7cbd8cd6fbd17e96
HeXiaoMing-Nous <56957328+HeXiaoMing-Nous@users.noreply.github.com>
2019-10-24 22:08:24 +0800

    Add files via upload

--- R/hello.R
+++ R/hello.R
@@ -94,9 +94,15 @@ intax <- function(a, b, d) {#计算个人所得税函数income tax for individua
   }
   预扣税额 <- c()
   本期应预扣税额 <- c()
+
   for (i in 1:length(d)){
     预扣税额[i] <- d[i]*b[maxid[i]]-速算扣除数[maxid[i]]
+    if(i==1){
+    本期应预扣税额[1] <- 预扣税额[1]
+    }
+    else(
     本期应预扣税额[i] <- 预扣税额[i]-预扣税额[i-1]
+    )
   }
   本期收入 <- d[1]
   if(length(d)!=1){
